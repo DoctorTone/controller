4b6c4a39c72afb8b3c234f096ac36a3952091041
DoctorTone <tony@drt-software.com>
2024-10-10 13:32:48 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Box } from "@react-three/drei";
+import { XR, createXRStore } from "@react-three/xr";
+
+const interactionsEnabled = true;
+const store = createXRStore({ controller: { right: true } });
+
+function App() {
+  const [VRSupported, setVRSupported] = useState(false);
+
+  useEffect(() => {
+    const isVRSupported = async () => {
+      if ("xr" in navigator) {
+        const supported =
+          await navigator.xr!.isSessionSupported("immersive-vr");
+        if (supported) {
+          setVRSupported(true);
+        }
+      }
+    };
+
+    isVRSupported();
+  }, []);
+
+  return (
+    <>
+      {VRSupported && (
+        <div id="VR" className="panel">
+          <button onClick={() => store.enterVR()}>Enter VR</button>
+        </div>
+      )}
+      <Canvas>
+        <XR store={store}>
+          <Box />
+        </XR>
+        <OrbitControls
+          makeDefault
+          enablePan={interactionsEnabled}
+          enableRotate={interactionsEnabled}
+          enableDamping={interactionsEnabled}
+        />
+      </Canvas>
+    </>
+  );
+}
+
+export default App;
